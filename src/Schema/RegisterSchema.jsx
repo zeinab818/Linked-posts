a458809f8a5aefd849fdc39d4d237763d00dc5db
Zainab Ali Mousa <safaali8118-email@example.com>
2025-08-31 23:14:03 +0300

    edited

--- src/Schema/RegisterSchema.jsx
+++ src/Schema/RegisterSchema.jsx
@@ -10,7 +10,10 @@ export const schema=zod.object({
         .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ ,'Email must be a valid address (e.g. name@example.com)'),
 
     password:zod.string().nonempty('Password Is Required')
-        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,'Password must be at least 8 characters, include uppercase, lowercase, number, and special character'),
+        .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*?#&]{8,}$/,
+        'Password must be at least 8 characters, include uppercase, lowercase, number, and special character'
+        ),
     
     rePassword:zod.string().nonempty('RePassword Is Required'),
     gender:zod.coerce.string().nonempty('Gender Is Required'),
